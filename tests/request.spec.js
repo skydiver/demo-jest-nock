c33cc41a2904e10c7054d6532e91a99c4fecf735
Martín M <skydiver@users.noreply.github.com>
2020-05-17 10:33:34 -0300

    using nock with sampledata

--- tests/request.spec.js
+++ tests/request.spec.js
@@ -1,4 +1,11 @@
+const nock = require('nock');
 const request = require('../request');
+
+const sampleData = require('../sampleData.json');
+
+nock('https://jsonplaceholder.typicode.com')
+  .get('/users')
+  .reply(200, sampleData);
 
 describe('testing using nock', () => {
   test('test users endpoint', async () => {
