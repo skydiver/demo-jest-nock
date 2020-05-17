1ed7c1ec4a0e37e4c8eff50601e1e9f6b2cb3e47
Martín M <skydiver@users.noreply.github.com>
2020-05-17 10:46:03 -0300

    added new test

--- tests/request.spec.js
+++ tests/request.spec.js
@@ -5,12 +5,30 @@ const sampleData = require('../sampleData.json');
 
 nock('https://jsonplaceholder.typicode.com')
   .get('/users')
-  .reply(200, sampleData);
+  .reply(200, sampleData)
+  .persist();
 
 describe('testing using nock', () => {
   test('test users endpoint', async () => {
     const response = await request();
     expect(typeof response).toBe('object');
     expect(response.length).toBe(10);
+  });
+
+  test('test user object', async () => {
+    const response = await request();
+    const userPosition = Math.floor(Math.random() * 10);
+    const user = response[userPosition];
+
+    expect(user).toMatchObject({
+      id: expect.any(Number),
+      name: expect.any(String),
+      address: expect.any(Object),
+      company: {
+        name: expect.any(String),
+        catchPhrase: expect.any(String),
+        bs: expect.any(String),
+      },
+    });
   });
 });
